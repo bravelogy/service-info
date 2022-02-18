bfc50c82c94ba5394ae930eab1197a2e63327d17
Jarek Machaj <jarekmachaj@users.noreply.github.com>
2022-02-18 19:16:28 +0100

    inital commit

---
+++ src/bravelogy.common.serviceinfo/ServiceInfoMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+
+namespace Bravelogy.Common.ServiceInfo
+{
+    public class ServiceInfoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ServiceInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            await httpContext.Response.WriteAsync("Hello from non-Map delegate. <p>");
+        }
+    }
+}
